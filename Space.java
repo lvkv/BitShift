341003a72f0945ba4d59ee6f7cc0be340facae45
Lukas Velikov <velikovlukas+github@gmail.com>
2016-11-07 23:07:46 -0500

    Package private methods and class

--- Space.java
+++ Space.java
@@ -1,28 +1,28 @@
-public class Space {
+class Space {
     private int value;
 
-    public Space(int v){
+    Space(int v){
         value = v;
     }
 
     // Adds value of otherSpace to current space, sets value of otherSpace to 0
-    public void add(Space otherSpace){
+    void add(Space otherSpace){
         this.setValue(this.getValue() + otherSpace.getValue());
         otherSpace.setValue(0);
     }
 
     // Sets Space value to newValue
-    public void setValue(int newValue){
+    void setValue(int newValue){
         value = newValue;
     }
 
     // Returns Space value as int
-    public int getValue(){
+    int getValue(){
         return value;
     }
 
     // Returns true if two spaces have the same value
-    public boolean equals(Space otherSpace){
+    boolean equals(Space otherSpace){
         return this.getValue() == otherSpace.getValue();
     }
 
