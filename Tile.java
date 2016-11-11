6c23ab2eb78718bcb68c0185e0d2135e597250df
Lukas Velikov <velikovlukas+github@gmail.com>
2016-11-10 21:54:10 -0500

    Colors planned out, font sizes+colors adjusted

--- Tile.java
+++ Tile.java
@@ -35,30 +35,63 @@ public class Tile extends GridPane{
             switch(intValue){
                 case 2:
                     border.setFill(Color.BISQUE);
+                    text.setFill(Color.GRAY);
+                    text.setFont(Font.font(72));
                     break;
                 case 4:
                     border.setFill(Color.WHEAT);
+                    text.setFill(Color.GRAY);
+                    text.setFont(Font.font(72));
                     break;
                 case 8:
+                    border.setFill(Color.TAN);
+                    text.setFill(Color.GRAY);
+                    text.setFont(Font.font(72));
                     break;
                 case 16:
+                    border.setFill(Color.LIGHTSALMON);
+                    text.setFill(Color.GRAY);
+                    text.setFont(Font.font(72));
                     break;
                 case 32:
+                    border.setFill(Color.DARKSALMON);
+                    text.setFill(Color.GRAY);
+                    text.setFont(Font.font(72));
                     break;
                 case 64:
+                    border.setFill(Color.SALMON);
+                    text.setFill(Color.WHITE);
+                    text.setFont(Font.font(72));
                     break;
                 case 128:
+                    border.setFill(Color.INDIANRED);
+                    text.setFill(Color.WHITE);
+                    text.setFont(Font.font(60));
                     break;
                 case 256:
+                    border.setFill(Color.INDIANRED);
+                    text.setFill(Color.WHITE);
+                    text.setFont(Font.font(60));
                     break;
                 case 512:
+                    border.setFill(Color.INDIANRED);
+                    text.setFill(Color.WHITE);
+                    text.setFont(Font.font(60));
                     break;
                 case 1024:
+                    border.setFill(Color.INDIANRED);
+                    text.setFill(Color.WHITE);
+                    text.setFont(Font.font(45));
                     break;
                 case 2048:
+                    border.setFill(Color.INDIANRED);
+                    text.setFill(Color.WHITE);
+                    text.setFont(Font.font(45));
                     break;
                 default:
                     border.setFill(Color.BISQUE);
+                    text.setFill(Color.GRAY);
+                    text.setFont(Font.font(72));
                     break;
             }
 
